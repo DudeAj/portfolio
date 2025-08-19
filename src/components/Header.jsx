d8969bba4fb160bf7ae050c5c21e6a199968df1d
Ajay Kumar Gangwar <74037417+DudeAj@users.noreply.github.com>
2025-08-19 12:35:54 +0530

    added menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,25 @@ const Header = () => {
         <span>.Ajay</span>
         <span className="text-purple-500">_</span>
       </p>
-      <nav className="flex justify-center gap-3 md:gap-10 items-center group">
+      <div className="w-full flex md:hidden justify-end">
+        <svg
+        className="cursor-pointer"
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height="24px"
+          width="24px"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="Menu_Fries">
+            <path d="M20.437,19.937c0.276,0 0.5,0.224 0.5,0.5c0,0.276 -0.224,0.5 -0.5,0.5l-16.874,0.002c-0.276,-0 -0.5,-0.224 -0.5,-0.5c-0,-0.276 0.224,-0.5 0.5,-0.5l16.874,-0.002Z"></path>
+            <path d="M20.437,11.5c0.276,-0 0.5,0.224 0.5,0.5c0,0.276 -0.224,0.5 -0.5,0.5l-10,0.001c-0.276,-0 -0.5,-0.224 -0.5,-0.5c-0,-0.276 0.224,-0.5 0.5,-0.5l10,-0.001Z"></path>
+            <path d="M20.437,3.062c0.276,-0 0.5,0.224 0.5,0.5c0,0.276 -0.224,0.5 -0.5,0.5l-16.874,0.001c-0.276,-0 -0.5,-0.224 -0.5,-0.5c-0,-0.276 0.224,-0.5 0.5,-0.5l16.874,-0.001Z"></path>
+          </g>
+        </svg>
+      </div>
+      <nav className="hidden md:flex justify-center gap-3 md:gap-10 items-center group">
         {menus.map((item, i) => (
           <a href={`#${item.to}`} key={i}>
             <div className="text-lg font-semibold relative text-white group-hover:text-zinc-500 hover:text-white transition-colors duration-300">
